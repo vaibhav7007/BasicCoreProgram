330df50ede4e602b23f0663e01767563d73eeaeb
vaibhav <vaibhavmehta524@gmail.com>
2023-02-08 23:34:28 +0530

    Swap Two Number program

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,7 +7,7 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter option given below");
-            Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOf2\n4:Harmonic Number\n5:PrimeFactors\n6:QuotientRemainder");
+            Console.WriteLine("1:FlipCoin\n2:LeapYear\n3:PowerOf2\n4:Harmonic Number\n5:PrimeFactors\n6:QuotientRemainder\n7:SwapNumber");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -35,6 +35,10 @@ namespace BasicCorePrograms
 
                     case 6:
                     QuotientRemainder.Value();
+                    break;
+
+                    case 7:
+                    SwapTwoNum.Result();
                     break;
             }
         }
