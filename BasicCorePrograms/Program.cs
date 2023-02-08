757b9218eab4fcf27bdad294996cc9f4867e9192
vaibhav <vaibhavmehta524@gmail.com>
2023-02-08 08:29:36 +0530

    Leap Year program

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,13 +7,18 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter option given below");
-            Console.WriteLine("1:FlipCoin");
+            Console.WriteLine("1:FlipCoin\n2:LeapYear");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.FindPercentage();
+                    break;
+
+                    case 2:
+                    LeapYear leapYear = new LeapYear();
+                    leapYear.FindLeap();
                     break;
             }
         }
