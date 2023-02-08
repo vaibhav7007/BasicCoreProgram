f6565137e9353a6d488eef01f2bdbd3a4d69114f
vaibhav <vaibhavmehta524@gmail.com>
2023-02-08 08:25:20 +0530

    Flip coin program

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter option given below");
+            Console.WriteLine("1:FlipCoin");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.FindPercentage();
+                    break;
+            }
+        }
+    }
+}
